57fffa82013fdfc1c6de1f3cd0c1e11eea67d2bc
timdoozy <thinds96@ufl.edu>
2017-11-08 07:42:05 -0500

    Trivia game project
    project group 24's trivia game
    Nicholas, Stephen, Dwain, Gerrad, Timothy, Matthew, and Sean

---
+++ triviagame.cpp
@@ -0,0 +1,8 @@
+//
+//  triviagame.cpp
+//  
+//
+//  Created by Timothy Hinds on 11/8/17.
+//
+
+#include "triviagame.hpp"
---
+++ triviagame.hpp
@@ -0,0 +1,13 @@
+//
+//  triviagame.hpp
+//  
+//
+//  Created by Timothy Hinds on 11/8/17.
+//
+
+#ifndef triviagame_hpp
+#define triviagame_hpp
+
+#include <stdio.h>
+
+#endif /* triviagame_hpp */
