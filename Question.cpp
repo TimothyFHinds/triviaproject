cf766945f259d0d049e8160d6ff47dc46392bd2f
timdoozy <thinds96@ufl.edu>
2017-11-15 08:33:37 -0500

    question header file

---
+++ Question.cpp
@@ -0,0 +1,18 @@
+/*
+ * Question.cpp
+ *
+ *  Created on: Nov 15, 2017
+ *      Author: tim
+ */
+
+#include "Question.h"
+
+Question::Question() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Question::~Question() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Question.h
@@ -0,0 +1,57 @@
+/*
+ * Question.h
+ *
+ *  Created on: Nov 15, 2017
+ *      Author: tim
+ */
+
+#ifndef QUESTION_H_
+#define QUESTION_H_
+
+#include <string>
+using namespace std;
+
+
+class Question
+{
+private:
+	string question;
+	string answer1;
+	string answer2;
+	string answer3;
+	string answer4;
+	int correctAnswer;
+
+public:
+	Question(const string &, const string &, const string &, const string &,
+			const string &, int);
+
+	void setQuestion(const string);
+	string getQuestion()const;
+
+	void setAnswer1(const string);
+	string getAnswer1()const;
+
+	void setAnswer2(const string);
+	string getAnswer2()const;
+
+	void setAnswer3(const string);
+	string getAnswer3()const;
+
+	void setAnswer1(const string);
+	string getAnswer1()const;
+
+	void setCorrect(int);
+	int getCorrect()const;
+
+	void displayInfo();
+
+
+	virtual ~Question();
+};
+
+
+
+
+
+#endif /* QUESTION_H_ */
